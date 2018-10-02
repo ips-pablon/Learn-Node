023cd32e1163e1e8d19780893a2cdb33c7cca187
Pablo Nunez <pablo.nunez@laureate.net>
2018-10-02 18:10:49 -0400

    Session 12

--- delicious/controllers/storeController.js
+++ delicious/controllers/storeController.js
@@ -1,5 +1,7 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
-    console.log(req.name);
     res.render('index');
 };
 
@@ -7,6 +9,8 @@ exports.addStore = (req, res) => {
     res.render('editStore', {title: 'Add Store'});
 };
 
-exports.createStore = (req, res) => {
-    res.json(req.body);
+exports.createStore = async (req, res) => {
+    const store = await (new Store(req.body)).save();
+    req.flash('success', `Successfully Created <strong>${store.name}.</strong> Care to leave a review?`);
+    res.redirect(`/store/${store.slug}`);
 };
