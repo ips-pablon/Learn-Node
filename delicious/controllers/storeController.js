6339bc7431ec5919a507576c7c27ed21ed648ba5
Pablo Nunez <pablo.nunez@laureate.net>
2018-10-08 16:56:26 -0400

    Session 14 Done

--- delicious/controllers/storeController.js
+++ delicious/controllers/storeController.js
@@ -14,3 +14,32 @@ exports.createStore = async (req, res) => {
     req.flash('success', `Successfully Created <strong>${store.name}.</strong> Care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+    // 1. Query the database for a list of all stores.
+    const stores = await Store.find();
+    res.render('stores', {title: 'Stores', stores});
+};
+
+exports.editStore = async (req, res) => {
+    // 1. Find the store given the ID
+    const store = await Store.findOne({ _id: req.params.id });
+
+    // 2. Confirm the are the owner of the store
+    // TODO
+
+    // 3. Render out the edit form so the user can update their store
+    res.render('editStore', {title: `Edit ${store.name}`, store});
+};
+
+exports.updateStore = async (req, res) => {
+    // 1. Find and update the store
+    const store = await Store.findOneAndUpdate({_id: req.params.id }, req.body, {
+        new: true, // Return the new store instead of the old one
+        runValidators: true // Force model to run required validations against this update
+    }).exec();
+
+    // 2 Redirect them to the store and tell them it worked
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href='/stores/${store.slug}'>View Store →</a>`);
+    res.redirect(`/stores/${store._id}/edit`);
+};
