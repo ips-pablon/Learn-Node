18c1db67333479385668880d2b41ea9f75566557
Pablo Nunez <pablo.nunez@laureate.net>
2018-10-31 17:11:01 -0400

    Session 27

--- delicious/models/User.js
+++ delicious/models/User.js
@@ -19,7 +19,9 @@ const userSchema = new Schema({
         type: String,
         required: 'Please Supply an email address',
         trim: true
-    }
+    },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
 });
 
 userSchema.virtual('gravatar').get(function() {
